9339b8a44ec04ee91b64bfe9813ff7d823db836e
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-02-03 22:34:07 +0700

    week01: (tugas2) implement discount logic

--- src/oop_00000113628_BagusKuncoroAdiYuwono/week01/GamesStore.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week01/GamesStore.kt
@@ -4,3 +4,5 @@ fun main() {
     val gameTitle = "Gathering Wives"
     val price = 700000
 }
+
+fun calculateDiscount(price: Int) = if (price > 500000) price - (price * 20/100) else price - (price * 10/100)

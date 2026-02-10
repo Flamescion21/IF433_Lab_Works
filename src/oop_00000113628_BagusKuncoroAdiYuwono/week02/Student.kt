16c205b0c7d2d7ca667336d218f6cdb2d3c24d5d
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-02-10 15:55:11 +0700

    week02: add secondary constructor

--- src/oop_00000113628_BagusKuncoroAdiYuwono/week02/Student.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week02/Student.kt
@@ -7,10 +7,14 @@ class Student(var name: String,
 ){
     init {
         if(nim.length != 5){
-            println("Warning: Onjek tercipta dengan NIM ($nim) yang tidak valid")
+            println("Warning: Objek tercipta dengan NIM ($nim) yang tidak valid")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem")
         } else{
             println("LOG: Objek student $name berhasil dialokasikan di memory")
         }
+    }
+
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matricilated"){
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
 }

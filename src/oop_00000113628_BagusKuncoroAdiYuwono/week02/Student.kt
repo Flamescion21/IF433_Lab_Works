f89999bd666795f67823453f58598d5c64101c81
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-02-10 15:33:25 +0700

    week02: add warning validation in init block

--- src/oop_00000113628_BagusKuncoroAdiYuwono/week02/Student.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week02/Student.kt
@@ -6,3 +6,12 @@ class Student(var name: String,
               var nim: String,
               var major: String
 ){
+    init {
+        if(nim.length != 5){
+            println("Warning: Onjek tercipta dengan NIM ($nim) yang tidak valid")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem")
+        } else{
+            println("LOG: Objek student $name berhasil dialokasikan di memory")
+        }
+    }
+}

8b50ab2d800744d5e7d15a20f7f6165589d74d3e
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-02-10 15:49:46 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week02/Main.kt
@@ -0,0 +1,29 @@
+package oop_00000113628_BagusKuncoroAdiYuwono.week02
+
+import java.util.Scanner
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukan NIM  (Wajib 5 karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+
+    if (nim.length !=5) {
+        println("ERROR: Pendaftaran di batalkan. NIM haris 5 karakter!")
+
+    } else {
+        print("Masukan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai")
+    }
+}

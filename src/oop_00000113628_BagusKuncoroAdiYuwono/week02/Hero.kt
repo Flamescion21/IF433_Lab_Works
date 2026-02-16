04c6e85a3b97425353ab1a95922130f8dd2f21ca
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-02-17 00:04:53 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week02/Hero.kt
@@ -0,0 +1,26 @@
+package oop_00000113628_BagusKuncoroAdiYuwono.week02
+
+class Hero(
+    val name: String,
+    var hp: Int = 100,
+    val baseDamage: Int
+){
+    fun attack(targetName: String){
+        println("$name Serang $targetName")
+    }
+
+    fun takeDamage(damage: Int){
+        hp -= damage
+        if(hp < 0){
+            hp = 0
+        }
+
+        println("$name terkena $damage damage.")
+        println("Status HP : $hp")
+    }
+
+
+    fun isAlive(): Boolean{
+        return hp > 0
+    }
+}

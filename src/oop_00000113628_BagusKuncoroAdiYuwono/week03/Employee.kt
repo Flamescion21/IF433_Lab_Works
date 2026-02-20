b1972257bee7e56edf25cb4bf58409dd8616b8e6
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-02-20 14:52:15 +0700

    week03: add private property

--- src/oop_00000113628_BagusKuncoroAdiYuwono/week03/Employee.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week03/Employee.kt
@@ -1,13 +1,24 @@
 package oop_00000113628_BagusKuncoroAdiYuwono.week03
 
-class Employee(val name: String){
-    var salary: Int = 0
-        set(value){
-            if(value < 0){
-                println("ERROR: Gaji tidak boleh negatif! diset ke 0")
-                field = 0
-            }else{
-                field = value
-            }
-        }
+private var performanceRating: Int = 3
+
+fun increasePerformance(){
+    performanceRating++
+    println("Kinerja $name meningkat! Rating: $performanceRating")
 }
+
+fun printStatus(){
+    println("Kinerja $name meningkat, Rating: $performanceRating")
+}
+
+    class Employee(val name: String) {
+        var salary: Int = 0
+            set(value) {
+                if (value < 0) {
+                    println("ERROR: Gaji tidak boleh negatif! diset ke 0")
+                    field = 0
+                } else {
+                    field = value
+                }
+            }
+    }

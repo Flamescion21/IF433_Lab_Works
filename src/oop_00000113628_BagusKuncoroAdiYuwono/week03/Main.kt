75b7a903536853a4362fd050450ce22181183bb0
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-02-20 15:09:38 +0700

    week03: (task 2) implement plater testing in main

--- src/oop_00000113628_BagusKuncoroAdiYuwono/week03/Main.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week03/Main.kt
@@ -11,9 +11,21 @@ package oop_00000113628_BagusKuncoroAdiYuwono.week03
     println("Pajak yang harus dibayar: ${e.tax}")
 }*/
 
-fun main(){
+/*fun main(){
     val weapon = Weapon("Weapon1", 100)
 
     println(weapon.damage)
     println(weapon.tier)
+}*/
+
+fun main(){
+    val player = Player("Yogi", 0)
+    println("Nama: ${player.username}")
+    println("level: ${player.level}")
+
+    player.addXp(50)
+    println("level baru: ${player.level}")
+
+    player.addXp(60)
+    println("level baru: ${player.level}")
 }

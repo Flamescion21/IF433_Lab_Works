381359775d1715d842636bd035b3b66e5f92ef9c
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-02-20 15:02:56 +0700

    week03: (task 1) create weapon class with validation

---
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week03/Weapon.kt
@@ -0,0 +1,27 @@
+package oop_00000113628_BagusKuncoroAdiYuwono.week03
+
+class Weapon(name: String, damage: Int){
+    val name: String = name
+    var damage: Int = damage
+        set(value){
+            if(value < 0){
+                println("Damage tidak bisa < 0")
+            }else if(value  > 1000){
+                println("Terlalu overpowered!")
+                field = 1000
+            }else{
+                field = value
+            }
+        }
+
+    val tier: String
+        get(){
+            if(damage > 800){
+                return "Legendary"
+            }else if(damage > 500){
+                return "Epic"
+            }else{
+                return "Common"
+            }
+        }
+}

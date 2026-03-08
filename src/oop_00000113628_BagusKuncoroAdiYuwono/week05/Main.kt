55bb07e6fb42b0ecf1c9d16c00dcb8b0481b4d1f
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-03-08 21:03:12 +0700

    week05: (task 2) implement polymorphism

--- src/oop_00000113628_BagusKuncoroAdiYuwono/week05/Main.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week05/Main.kt
@@ -1,11 +1,22 @@
 package oop_00000113628_BagusKuncoroAdiYuwono.week05
 
 fun main(){
-    val dosenObject = Dosen("YogiDosen", "02932")
-    val adminObject = Admin("YogiAdmin")
-
+    val dosenObject = Dosen("Bagus K", "2136")
+    val adminObject = Admin("Joko")
+    val eWalletObject = EWallet("Bagus", 50000.0)
+    val creditCardObject = creditCard("BK", 100000.0)
+    val PaymentMethods: List<PaymentMethod> = listOf(eWalletObject, creditCardObject)
     val daftarPegawai: List<Pegawai> = listOf(dosenObject, adminObject)
     val math = MathHelper()
+
+
+
+
+    for(PaymentMethod in PaymentMethods) {
+        PaymentMethod.processPayment(75000.0)
+    }
+
+
     for(pegawai in daftarPegawai){
         pegawai.bekerja()
 

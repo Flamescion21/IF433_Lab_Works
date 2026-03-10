fdc5bcffc6e2bf687f41360d6e0d47566b5c21e6
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-03-10 21:00:21 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week06/SmartInterfaces.kt
@@ -0,0 +1,16 @@
+package oop_00000113628_BagusKuncoroAdiYuwono.week06
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+    fun stopRecord() { println("Perekaman dihentikan dan disimpan ke Cloud.") }
+}

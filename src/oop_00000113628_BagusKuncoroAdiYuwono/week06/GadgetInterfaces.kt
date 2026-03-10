0bce60495c7e8e6c5590965c0eeb039c04837ced
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-03-10 15:28:31 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000113628_BagusKuncoroAdiYuwono.week06
+
+interface Camera {
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif")
+    }
+}
+
+interface  Phone {
+    fun turnOn(){
+        println("Sinyal Seluler mencari jaringan")
+    }
+}

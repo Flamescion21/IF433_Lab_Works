0d86407fd96fb5df07b601bbe4f7892c6b47f183
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-04-14 15:47:05 +0700

    week08: test safe casting in main

--- src/oop_00000113628_BagusKuncoroAdiYuwono/week08/UserProfile.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week08/UserProfile.kt
@@ -1,8 +1,8 @@
 package oop_00000113628_BagusKuncoroAdiYuwono.week08
 
 class UserProfile (
-    val name: String
-    val email: String?
+    val name: String,
+    val email: String?,
     val phone: String? = null
 
 )

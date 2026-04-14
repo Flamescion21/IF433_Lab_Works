90666208a54b5644342d109a87f8e5a177536cab
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-04-14 15:38:45 +0700

    week08: implement safe call with let block

--- src/oop_00000113628_BagusKuncoroAdiYuwono/week08/Main.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week08/Main.kt
@@ -6,4 +6,15 @@ fun main(){
 
     val desination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("tujuan pengiriman: $desination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val valiOrder = Order(null, 250000)
+
+    val receipt = valiOrder.totalPrice?.let { price ->
+
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di set!"
+
+    println(receipt)
 }

2012947ddd70ed5afef0f666457073d599df1ea4
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-04-21 00:34:03 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week08/JavaPaymentService.java
@@ -0,0 +1,6 @@
+package oop_00000113628_BagusKuncoroAdiYuwono.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){
+        return "TRX-" + productId + "-SUCCESS";}
+}

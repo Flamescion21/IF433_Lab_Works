075462de553fc3112955277f1bfd1a47c0ff175a
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-04-21 00:32:19 +0700

    week08: (task) implement robust ApiPraser using Elvis and requireNotNULL

---
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week08/ApiParser.kt
@@ -0,0 +1,23 @@
+package oop_00000113628_BagusKuncoroAdiYuwono.week08
+
+class ApiParser(){
+    fun parseProduct(rawJson: Map<String, Any?>): Product?{
+        val id = requireNotNull(rawJson["id"]){"API invalid: ID!"}.toString()
+        val name = requireNotNull(rawJson["name"]){"API invalid: Name!"}.toString()
+
+        val type = rawJson["type"] as? String
+
+        return when (type){
+            "ELECTRONIC" -> {
+                val warranty = rawJson["warranty"] as? Int ?: 12
+                Electronic(id, name, warranty)
+            }
+
+            "CLOTHING" -> {
+                val size = rawJson["size"] as? String ?: "All size"
+                Clothing(id, name, size)
+            }
+            else -> null
+        }
+    }
+}

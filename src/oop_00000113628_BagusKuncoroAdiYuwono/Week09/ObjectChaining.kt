41a32311b447fa9abf08e3d425d624a7dddabff2
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-04-21 15:36:39 +0700

    week09: create Student data class and list

---
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/Week09/ObjectChaining.kt
@@ -0,0 +1,12 @@
+package oop_00000113628_BagusKuncoroAdiYuwono.Week09
+
+data class Student(val name: String, val gpa: Double)
+
+fun main(){
+    val students = listOf(
+        Student("andi", 3.2),
+        Student("budi", 3.9),
+        Student("citra", 2.8),
+        Student("dewi", 3.7),
+    )
+}

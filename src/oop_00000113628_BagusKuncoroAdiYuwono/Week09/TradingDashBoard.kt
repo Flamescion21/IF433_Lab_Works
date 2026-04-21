4ed188f4d813189cdc0639803933aaa1d004e3c1
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-04-21 15:43:47 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/Week09/TradingDashBoard.kt
@@ -0,0 +1,13 @@
+package oop_00000113628_BagusKuncoroAdiYuwono.Week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 15, -10.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 25, 20.0, "CLOSED"),
+        TradeLog("XRPUSDT", "LONG", 5, 0.0, "OPEN"),
+        TradeLog("BNBUSDT", "SHORT", 8, -2.5, "OPEN"),
+        TradeLog("SOLUSDT", "LONG", 12, 8.3, "CLOSED")
+    )
+}

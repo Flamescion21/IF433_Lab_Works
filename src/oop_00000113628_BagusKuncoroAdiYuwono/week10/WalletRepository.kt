23fac7b195f6e2afc2153460064e1faba437cb2e
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-04-28 15:20:18 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000113628_BagusKuncoroAdiYuwono/week10/WalletRepository.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week10/WalletRepository.kt
@@ -3,10 +3,18 @@ package oop_00000113628_BagusKuncoroAdiYuwono.week10
 class WalletRepository<T>(){
     private val items =  mutableListOf<T>()
 
+    interface Named {
+        val name: String
+    }
+
     fun add(item: T){
         items.add(item)
     }
     fun getAll(): List<T>{
         return items
+    }
+
+    fun <T : Named> List<T>.findByName(query: String): List<T> {
+        return this.filter { it.name.contains(query, ignoreCase = true) }
     }
 }

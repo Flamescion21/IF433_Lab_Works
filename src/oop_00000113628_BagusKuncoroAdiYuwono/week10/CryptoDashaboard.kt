a0d0eaaa098ae7d16bfb5299fc7ba31988e7890f
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-04-28 15:42:50 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000113628_BagusKuncoroAdiYuwono/week10/CryptoDashaboard.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/week10/CryptoDashaboard.kt
@@ -1,6 +1,7 @@
 package oop_00000113628_BagusKuncoroAdiYuwono.week10
 
 val coinRepo = WalletRepository<Coin>()
+val response = ApiResponse("200 OK", coinRepo.getAll())
 fun main(){
     coinRepo.add(Coin("BTC", 64000.0))
     coinRepo.add(Coin("ETH", 65000.0))

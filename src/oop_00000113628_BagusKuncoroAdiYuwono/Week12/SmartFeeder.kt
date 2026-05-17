e4000e2342bcf7c22da7c5788adf47845d7d160e
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-05-17 10:08:15 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/Week12/SmartFeeder.kt
@@ -0,0 +1,8 @@
+package oop_00000113628_BagusKuncoroAdiYuwono.Week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0){
+        "porsi kibble harus lebih dari 0gr"
+    }
+    return availableGram - requestedGram
+}

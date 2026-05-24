9ebcc0ee08f6a78faf5cf516fe3bba0c8cee5411
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-05-24 19:00:51 +0700

    week13: implement printWriter with safe use block

--- src/oop_00000113628_BagusKuncoroAdiYuwono/Week13/ResourceHandling.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/Week13/ResourceHandling.kt
@@ -10,4 +10,13 @@ fun main() {
     writer.println("Log 2: Menulis data pengguna...")
     writer.close()
     println("Proses penulisan unsafe selesai")
+
+    println("\nTest safe resource handling")
+    val safeFile = File("safeLogs.txt")
+    safeFile.printWriter().use { out ->
+        for (i in 1..100) {
+            out.println("Safe Log entry #$i: System status OK")
+        }
+        println("100 baris log berhasil di generate dengan aman")
+    }
 }

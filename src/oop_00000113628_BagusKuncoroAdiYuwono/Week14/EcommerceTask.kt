133c74ef617c99f474e09d08a96974de442fb612
Flamescion21 <baguskuncoroadiyuwono@gmail.com>
2026-05-28 18:19:38 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_00000113628_BagusKuncoroAdiYuwono/Week14/EcommerceTask.kt
+++ src/oop_00000113628_BagusKuncoroAdiYuwono/Week14/EcommerceTask.kt
@@ -1,10 +1,36 @@
 package oop_00000113628_BagusKuncoroAdiYuwono.Week14
-
 import java.io.File
 
-class BadOrderProcessor {
-    // VIOLATION: Hardcoded File I/O (DIP), Melakukan kalkulasi + I/O + Notifikasi sekaligus
-    private val file = File("orders.csv")
+interface OrderRepository {
+    fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
+}
+
+interface NotificationService {
+    fun sendNotification(message: String)
+}
+
+class CsvOrderRepository(private val fileName: String = "orders.csv") : OrderRepository {
+    private val file = File(fileName)
+
+    override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
+        file.bufferedWriter().use { writer ->
+            writer.append("$itemName,$finalPrice,$customerType\n")
+        }
+    }
+}
+
+class EmailNotifier : NotificationService {
+    override fun sendNotification(message: String) {
+        println("Email terkirim: $message")
+    }
+}
+
+class SafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+) {
+
+
 
     fun processOrder(itemName: String, basePrice: Double, customerType: String) {
 
@@ -17,10 +43,7 @@ class BadOrderProcessor {
 
         println("Memproses pesanan $itemName seharga $finalPrice")
 
-        // VIOLATION SRP/DIP: Menulis file langsung di class bisnis
-        file.appendText("$itemName,$finalPrice,$customerType\n")
-
-        // VIOLATION SRP/DIP: Notifikasi terikat kuat dengan sistem order
-        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+        repo.saveOrder(itemName, finalPrice, customerType)
+        notifier.sendNotification("Pesanan $itemName Anda telah dikonfirmasi!")
     }
 }
